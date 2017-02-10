b305a9a71a1028cc61bf4c9ebea2e507144fbbc5
Joshua Barton <FooBartn@users.noreply.github.com>
2017-02-10 00:09:01 -0600

    Create Get-EsxNicDriver.ps1

---
+++ Get-EsxNicDriver.ps1
@@ -0,0 +1,27 @@
+function Get-EsxNicDriver () {
+    [CmdletBinding()]
+    param (
+        # VMHost
+        [Parameter(Mandatory)]
+        [string]
+        $Name
+    )
+
+    $VMHost = Get-VMHost @PSBoundParameters
+    $EsxCli = $VMHost | Get-EsxCli
+    
+    $EnicRaw = ($EsxCli.System.Module.Get('enic') | Select-Object Version).Version
+    $EnicVer = $EnicRaw.Split(',') | Select-Object -First 1
+    $Enic = $EnicVer.Replace('Version ','')
+    
+
+    $FnicRaw = ($EsxCli.System.Module.Get('fnic') | Select-Object Version).Version
+    $FnicVer = $FnicRaw.Split(',') | Select-Object -First 1
+    $Fnic = $FnicVer.Replace('Version ','')
+
+    [PsCustomObject]@{
+        Host = $VMHost.Name
+        Enic = $Enic
+        Fnic = $Fnic
+    }
+}
